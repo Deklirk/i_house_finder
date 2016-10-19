f75bc5364ca6a0c41b68d1e04f32ef0ae66d27a0
bed Juma <abedjuma@gmail.com>
2016-10-19 09:26:02 +0300

    i_house_finder app, first commit

---
+++ i_house_finder/urls.py
@@ -0,0 +1,22 @@
+"""i_house_finder URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^i_house_blog/', include('i_house_blog.urls', namespace='i_house_blog', app_name='i_house_blog')),
+]
